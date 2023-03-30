4a28dfa1e56017ac613b974d29f281a7767f3460
Giaco <dadigi@users.noreply.github.com>
2023-03-30 16:47:00 +0200

    Update settings.py
    Fix for issue 8?

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,9 @@
 import uuid
 from pydantic import BaseSettings
-
+import os
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str = ""    # The OpenAI API key
+    OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]    # The OpenAI API key
     DRY_RUN: bool = True        # If true, don't send to the OpenAI API
     SYSTEM_PROMPT: str = ""     # the system role prompt part to prime GPT in the right direction.
     SESSION_UUID: uuid.UUID = uuid.uuid4()  # the "End-user ID" field for the OpenAI API
